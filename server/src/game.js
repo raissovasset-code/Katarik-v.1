86013b1c105901210c7d9ebd3a612703eb4bcfad
raissovasset-code <raissovasset@gmail.com>
2026-06-08 14:18:02 +0500

    Update game.js

--- server/src/game.js
+++ server/src/game.js
@@ -353,11 +353,12 @@ export function publicGameState(game, viewerId) {
     hand: me ? me.hand : [],
 
     players: game.players.map(p => ({
-      id: p.id,
-      name: p.name,
-      active: p.active,
-      handCount: p.hand.length,
-    })),
+  id: p.id,
+  name: p.name,
+  active: p.active,
+  handCount: p.hand.length,
+  pogonRank: p.pogonRank,
+})),
 
     burned: undefined,
   };
