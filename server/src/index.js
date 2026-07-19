686515786282027d38091c42323f689c17de1cb4
raissovasset-code <raissovasset@gmail.com>
2026-07-19 13:05:50 +0600

    OPS-03: serve storage health check under API route

--- server/src/index.js
+++ server/src/index.js
@@ -63,7 +63,7 @@ function hasConnectedPlayers(roomId) {
   return false;
 }
 
-app.get('/', (req, res) => {
+app.get('/api/health', (req, res) => {
   res.json({
     name: 'Katarik server',
     status: 'ok',
